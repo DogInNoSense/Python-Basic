bab531b1521703228566291719d21a36a53f129e
xyz <xieyinzhangl@gmail.com>
2022-11-20 17:40:57 +0800

    first commit

--- chapter17/实操案例13/demo2.py
+++ chapter17/实操案例13/demo2.py
@@ -1,34 +1,45 @@
 class Car(object):
-    def __init__(self,type,no):
-        self.type=type
-        self.no=no
+    def __init__(self, type, no):
+        self.type = type
+        self.no = no
+
     def start(self):
         pass
+
     def stop(self):
         pass
 
+
 class Taxi(Car):
-    def __init__(self,type,no,company):
-        super().__init__(type,no)
-        self.company=company
+    def __init__(self, type, no, company):
+        super().__init__(type, no)
+        self.company = company
+
     def start(self):
         print('乘客你好！')
         print(f'我是{self.company}出租车公司的,我的车牌是{self.no},请问你要去哪里')
+
     def stop(self):
         print('目的地到了，请您付款下车，欢迎下次乘坐')
+
+
 class FamilyCar(Car):
-    def __init__(self,type,no,name):
-        super().__init__(type,no)
-        self.name=name
+    def __init__(self, type, no, name):
+        super().__init__(type, no)
+        self.name = name
+
     def stop(self):
         print('目的地到了，我们去玩吧')
+
     def start(self):
         print(f'我是{self.name},我的汽车我做主')
+
+
 if __name__ == '__main__':
-    taxi=Taxi('上海大众','京A9765','长城')
+    taxi = Taxi('上海大众', '京A9765', '长城')
     taxi.start()
     taxi.stop()
-    print('_'*30)
-    familycar=FamilyCar('广汽丰田','京B88888','武大郎')
+    print('_' * 30)
+    familycar = FamilyCar('广汽丰田', '京B88888', '武大郎')
     familycar.start()
     familycar.stop()
